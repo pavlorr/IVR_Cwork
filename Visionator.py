6cbf8d2614b20305376440ab8e9db9351474affd
pavlosgounaris <pggounaris@gmail.com>
2021-11-18 21:09:42 +0200

    Template matching implemented & tested for Yellow Need to create Green, Red, Blue templates & ammend directories

--- Visionator.py
+++ Visionator.py
@@ -4,9 +4,14 @@ import cv2
 import os
 import numpy as np
 
-TEST_IMAGE_CAM_1_FILE = os.path.join(os.path.dirname(__file__), 'Test_files/cam_1.png')
-TEST_IMAGE_CAM_2_FILE = os.path.join(os.path.dirname(__file__), 'Test_files/cam_2.png')
+TEST_IMAGE_CAM_1_FILE = os.path.join(os.path.dirname(__file__), 'Test_files/cam_2_invest.png')
+TEST_IMAGE_CAM_1 = cv2.imread(TEST_IMAGE_CAM_1_FILE)
+TEST_IMAGE_CAM_2_FILE = os.path.join(os.path.dirname(__file__), 'Test_files/cam_2_baseline.png')
+TEST_IMAGE_CAM_2 = cv2.imread(TEST_IMAGE_CAM_2_FILE)
 YELLOW_TEMPLATE = cv2.imread(os.path.join(os.path.dirname(__file__), 'Test_files/yellow_template.PNG'), 0)
+RED_TEMPLATE = cv2.imread(os.path.join(os.path.dirname(__file__), 'Test_files/red_template.PNG'), 0)
+BLUE_TEMPLATE = cv2.imread(os.path.join(os.path.dirname(__file__), 'Test_files/blue_template.PNG'), 0)
+GREEN_TEMPLATE = cv2.imread(os.path.join(os.path.dirname(__file__), 'Test_files/green_template.PNG'), 0)
 LINK_1_LENGTH = 4.0
 LINK_1_PIXEL_LENGTH = 105
 LINK_2_LENGTH = 0.0
@@ -31,21 +36,22 @@ def detect_blob(img: np.ndarray, lower: np.ndarray, upper: np.ndarray):
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.dilate(mask, kernel, iterations=3)
     mask = cv2.erode(mask, kernel, iterations=3)
-    M = cv2.moments(mask)
-    cx = int(M['m10'] / M['m00'])
-    cy = int(M['m01'] / M['m00'])
-    return np.array([cx, cy])
+    return mask
 
 
-def helper(img, lower, upper):
-    mask = cv2.inRange(img, lower, upper)
-    kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.dilate(mask, kernel, iterations=3)
-
-
-def pixel_to_meter(center_1, center_2, length):
-    dist = np.sum((center_1 - center_2) ** 2)
-    return length / np.sqrt(dist)
+def get_best(lower: np.ndarray, upper: np.ndarray, template: np.ndarray):
+    cam1_yellow = detect_blob(TEST_IMAGE_CAM_1, lower, upper)
+    cam_2_yellow = detect_blob(TEST_IMAGE_CAM_2, lower, upper)
+    w, h = template.shape[::-1]
+    cam_1_tmpl_mtch = cv2.matchTemplate(cam1_yellow, template, cv2.TM_CCORR)
+    cam_2_tmpl_mtch = cv2.matchTemplate(cam_2_yellow, template, cv2.TM_CCOEFF)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(cam_1_tmpl_mtch)
+    min_val2, max_val2, min_loc2, max_loc2 = cv2.minMaxLoc(cam_2_tmpl_mtch)
+    if max_val > max_val2:
+        center_of_blob = [max_loc[0] + w/2, max_loc[1] + h/2]
+    else:
+        center_of_blob = [max_loc2[0] + w/2, max_loc2[1] + h/2]
+    return center_of_blob
 
 
 def calc_angle():
@@ -64,32 +70,11 @@ def calc_3d_coord() -> np.ndarray:
     pass
 
 
-# def template_match(img: np.ndarray, lower: np.ndarray, upper: np.ndarray, template: np.ndarray = None) -> np.ndarray:
-#     blob_centre = detect_blob(img, lower, upper)
-#     mask = cv2.inRange(img, lower, upper)
-#     res = cv2.matchTemplate(mask, template, 0)
-#     print(template.shape)
-#     exit()
-#     ROI = mask[int(blob_centre[1] - template.shape[0] / 2): int(blob_centre[1] + template.shape[0] / 2) + 1,
-#           int(blob_centre[0] - template.shape[1] / 2): int(blob_centre[0] + template.shape[1] / 2) + 1]
-#     ROI = ROI[0:template.shape[0], 0:template.shape[1]]
-#     dist = cv2.distanceTransform(cv2.bitwise_not(ROI), cv2.DIST_L2, 0)
-#     res = dist*template[0:dist.shape[0], 0:dist.shape[1]]
-#     pass
-
-
 def main():
-    cam_1_img = cv2.imread(TEST_IMAGE_CAM_1_FILE, 1)
-    cam_2_img = cv2.imread(TEST_IMAGE_CAM_2_FILE, 1)
-    cam1_green_pix = detect_blob(cam_1_img, GREEN_LOWER, GREEN_UPPER)  # [0,0] point for cam 1
-    cam2_green_pix = detect_blob(cam_2_img, GREEN_LOWER, GREEN_UPPER)  # [0,0] point for cam 2
-    cam1_yellow_pix = detect_blob(cam_1_img, YELLOW_LOWER, YELLOW_UPPER)
-    cam2_yellow_pix = detect_blob(cam_2_img, YELLOW_LOWER, YELLOW_UPPER)
-    #template_match(cam_1_img, YELLOW_LOWER, YELLOW_UPPER, YELLOW_TEMPLATE)
-    best_blue_coords = None
-    best_red_coords = None
-    best_green_coords = None
-    best_yellow_coords = None
+    best_blue_coords = get_best(BLUE_LOWER, BLUE_UPPER, BLUE_TEMPLATE)
+    best_red_coords = get_best(RED_LOWER, RED_UPPER, RED_TEMPLATE)
+    best_green_coords = get_best(GREEN_LOWER, GREEN_UPPER, GREEN_TEMPLATE)
+    best_yellow_coords = get_best(YELLOW_LOWER, YELLOW_UPPER, YELLOW_TEMPLATE)
 
 
 if __name__ == '__main__':
