98a7bbb2613b51a680c459ae1094d5dbb3ac0885
pavlosgounaris <pggounaris@gmail.com>
2021-11-21 15:37:48 +0200

    Publicator updated to publish once & do things manually. Also improved the code

--- Publicator.py
+++ Publicator.py
@@ -2,6 +2,7 @@
 
 import rospy
 import math
+import sys
 from std_msgs.msg import Float64
 
 
@@ -10,48 +11,35 @@ JOINT_1_TOPIC = '/robot/joint1_position_controller/command'
 JOINT_2_TOPIC = '/robot/joint2_position_controller/command'
 JOINT_3_TOPIC = '/robot/joint3_position_controller/command'
 JOINT_4_TOPIC = '/robot/joint4_position_controller/command'
+JOINT_1 = 1
+JOINT_2 = 2
+JOINT_3 = 3
+JOINT_4 = 4
 
 
-class JointHandler:
+def joint_angle(joint_no: int, time: float) -> Float64:
     """
-    Class to control Joints & calculation
-    Attributes
-    ----------
-    pkg : Float64()
-        controls the return value
-    t : Float64
-        time used to calculate joint movements
-
-    Methods
-    -------
-    joint1 : Float64()
-        updates the pkg attribute with the movement associated to joint 1 and returns it - currently fixed
-    joint2 : Float64()
-        updates the pkg attribute with the movement associated to joint 2 and returns it
-    joint3 : Float64()
-        updates the pkg attribute with the movement associated to joint 3 and returns it
-    joint4 : Float64()
-        updates the pkg attribute with the movement associated to joint 4 and returns it
+    Calculates angle of rotation for each joint based on exercise description & given time
+    @todo: Would like to automate the publication & subscription but not possible in the time remaining
+    :param joint_no: Number of joint to be rotated
+    :type: int
+    :param time: time in seconds given to base the rotation on
+    :type: float
+    :return: the type of variable & angle of rotation for the given axis & time
+    :rtype: std_msgs.msg.Float64
     """
-    def __init__(self, t: Float64):
-        self.pkg = Float64()
-        self.t = t
-
-    def joint1(self) -> Float64:
-        # joint 1 not used for Q1
+    pkg = Float64()
+    if joint_no == 1:
         pass
-
-    def joint2(self) -> Float64:
-        self.pkg.data = (math.pi/2) * math.sin(math.pi*self.t/15)
-        return self.pkg
-
-    def joint3(self) -> Float64:
-        self.pkg.data = (math.pi/2) * math.sin(math.pi*self.t/20)
-        return self.pkg
-
-    def joint4(self) -> Float64:
-        self.pkg.data = (math.pi/2) * math.sin(math.pi*self.t/18)
-        return self.pkg
+    elif joint_no == 2:
+        pkg.data = (math.pi/2) * math.sin(math.pi*time/15)
+    elif joint_no == 3:
+        pkg.data = (math.pi/2) * math.sin(math.pi*time/20)
+    elif joint_no == 4:
+        pkg.data = (math.pi/2) * math.sin(math.pi*time/18)
+    else:
+        raise TypeError('Not supported joint_no: ' + str(joint_no))
+    return pkg
 
 
 class RadianPublicator:
@@ -86,25 +74,21 @@ class RadianPublicator:
         self.publicator_joint2 = rospy.Publisher(JOINT_2_TOPIC, Float64, queue_size=1)
         self.publicator_joint3 = rospy.Publisher(JOINT_3_TOPIC, Float64, queue_size=1)
         self.publicator_joint4 = rospy.Publisher(JOINT_4_TOPIC, Float64, queue_size=1)
-        self.rate = rospy.Rate(0.5)
 
-    def callback(self):
-        while not rospy.is_shutdown():
-            for i in range(30):
-                jh = JointHandler(i/2)
-                # joint 1 is fixed, so no publishing
-                # a better way of publishing can be found but does not really matter
-                #self.publicator_joint1.publish(JointHandler().pkg)
-                self.publicator_joint2.publish(jh.joint2())
-                self.publicator_joint3.publish(jh.joint3())
-                self.publicator_joint4.publish(jh.joint4())
-                self.rate.sleep()
+    def callback(self, seconds: float):
+        if not rospy.is_shutdown():
+            # joint 1 is fixed, so no publishing
+            # a better way of publishing can be found but does not really matter
+            #self.publicator_joint1.publish(JointHandler().pkg)
+            self.publicator_joint2.publish(joint_angle(JOINT_2, seconds))
+            self.publicator_joint3.publish(joint_angle(JOINT_3, seconds))
+            self.publicator_joint4.publish(joint_angle(JOINT_4, seconds))
 
 
 # run the code if the node is called
 if __name__ == '__main__':
     try:
         p = RadianPublicator()
-        p.callback()
+        p.callback(float(sys.argv[1]))
     except rospy.ROSInterruptException:
         pass
